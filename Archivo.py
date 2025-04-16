8c1a52f6e512cd575c84114d0a4ba6969948cc56
JoacoIbarborde <joacoibar2001@gmail.com>
2025-04-16 19:49:15 -0300

    tercer version agrego variables a dos archivos

--- Archivo.py
+++ Archivo.py
@@ -2,3 +2,5 @@ nombre = "Patricio"
 edad = 21
 print(f"Hola, soy {nombre} y tengo {edad} años")
 tieneMascota = True
+cantidadMascotas = 1
+nombreMascota = "Gato"
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 nombre = "Gisele"
 edad = 36
 print(f"Hola, soy {nombre} y tengo {edad} años")
+tieneMascota = True
+cantidadMascotas = 4
+nombreMascota = "Gatos cochinos"
