41aca79857bda23998950d03864efc1af253fd25
JoacoIbarborde <joacoibar2001@gmail.com>
2025-04-16 21:13:01 -0300

    feature/clase3 agrego condicional de ingreso al sistema

--- Archivo.py
+++ Archivo.py
@@ -2,5 +2,10 @@ nombre = "Patricio"
 edad = 21
 print(f"Hola, soy {nombre} y tengo {edad} años")
 tieneMascota = True
-cantidadMascotas = 1
-nombreMascota = "Gato"
+totalMascotas = 1
+tipoMascota = "Gato"
+
+if edad >= 18:
+    print("Puede ingresar al sistema")
+else:
+    print("No puede ingresar al sistema")
